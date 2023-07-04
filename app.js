ca531a8a8cfc1a10739df543ee14403dd46e9f81
Bodyguard007 <102570257+Bodyguard007@users.noreply.github.com>
2023-07-04 17:03:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ let client = new OSS({
 
   region: 'oss-cn-hangzhou',
   accessKeyId: 'LTAI5tDqZjgNQ2HCbi6h2nHa',
-  accessKeySecret: 'DemoForTestZyA4QlPvr7iLH8fCaaJ',
+  accessKeySecret: 'long_18710262313',
   
 });
