fb5c925e310949ec930ff0a07496d5adf4444816
Long <102570257+Bodyguard007@users.noreply.github.com>
2023-07-05 09:59:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ let client = new OSS({
 
   region: 'oss-cn-hangzhou',
   accessKeyId: 'LTAI5tDqZjgNQ2HCbi6h2nHa',
-  accessKeySecret: 'DemoForTestZyA4QlPvr7iLH8fCaa0',
+  accessKeySecret: 'DemoForTestZyA4QlPvr7iLH8fCaaJ',
   
 });
